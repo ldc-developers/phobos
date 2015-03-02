db001ed791b5433272a65a5bc60ee1ab553880f7
Kevin Brogan <kevin@brogan.ca>
2015-03-02 13:18:36 -0800

    Convert constants to floating point. hex constants are not supported in LDC. All constants have enough significant figures for the compiler to create binary exact representations to within the closest ULP. (Unit in the last place) Remove mathematical operations from constants that might introduce rounding errors.

--- std/math.d
+++ std/math.d
@@ -431,23 +431,25 @@ T floorImpl(T)(T x) @trusted pure nothrow @nogc
 
 public:
 
-// Values obtained from Wolfram Alpha. 116 bits ought to be enough for anybody.
-// Wolfram Alpha LLC. 2011. Wolfram|Alpha. http://www.wolframalpha.com/input/?i=e+in+base+16 (access July 6, 2011).
-enum real E =          0x1.5bf0a8b1457695355fb8ac404e7a8p+1L; /** e = 2.718281... */
-enum real LOG2T =      0x1.a934f0979a3715fc9257edfe9b5fbp+1L; /** $(SUB log, 2)10 = 3.321928... */
-enum real LOG2E =      0x1.71547652b82fe1777d0ffda0d23a8p+0L; /** $(SUB log, 2)e = 1.442695... */
-enum real LOG2 =       0x1.34413509f79fef311f12b35816f92p-2L; /** $(SUB log, 10)2 = 0.301029... */
-enum real LOG10E =     0x1.bcb7b1526e50e32a6ab7555f5a67cp-2L; /** $(SUB log, 10)e = 0.434294... */
-enum real LN2 =        0x1.62e42fefa39ef35793c7673007e5fp-1L; /** ln 2  = 0.693147... */
-enum real LN10 =       0x1.26bb1bbb5551582dd4adac5705a61p+1L; /** ln 10 = 2.302585... */
-enum real PI =         0x1.921fb54442d18469898cc51701b84p+1L; /** $(_PI) = 3.141592... */
-enum real PI_2 =       PI/2;                                  /** $(PI) / 2 = 1.570796... */
-enum real PI_4 =       PI/4;                                  /** $(PI) / 4 = 0.785398... */
-enum real M_1_PI =     0x1.45f306dc9c882a53f84eafa3ea69cp-2L; /** 1 / $(PI) = 0.318309... */
-enum real M_2_PI =     2*M_1_PI;                              /** 2 / $(PI) = 0.636619... */
-enum real M_2_SQRTPI = 0x1.20dd750429b6d11ae3a914fed7fd8p+0L; /** 2 / $(SQRT)$(PI) = 1.128379... */
-enum real SQRT2 =      0x1.6a09e667f3bcc908b2fb1366ea958p+0L; /** $(SQRT)2 = 1.414213... */
-enum real SQRT1_2 =    SQRT2/2;                               /** $(SQRT)$(HALF) = 0.707106... */
+// Values obtained from Wolfram Alpha.
+// http://www.wolframalpha.com/input/?i=pi
+// http://www.wolframalpha.com/input/?i=e in decimal
+// etc
+enum real E =          2.718281828459045235360287; /** e = 2.718281... */
+enum real LOG2T =      3.321928094887362347870319; /** $(SUB log, 2)10 = 3.321928... */
+enum real LOG2E =      1.442695040888963407359924; /** $(SUB log, 2)e = 1.442695... */
+enum real LOG2 =       0.301029995663981195213738; /** $(SUB log, 10)2 = 0.301029... */
+enum real LOG10E =     0.434294481903251827651128; /** $(SUB log, 10)e = 0.434294... */
+enum real LN2 =        0.693147180559945309417232; /** ln 2  = 0.693147... */
+enum real LN10 =       2.302585092994045684017991; /** ln 10 = 2.302585... */
+enum real PI =         3.141592653589793238462643; /** $(_PI) = 3.141592... */
+enum real PI_2 =       1.570796326794896619231321; /** $(PI) / 2 = 1.570796... */
+enum real PI_4 =       0.785398163397448309615660; /** $(PI) / 4 = 0.785398... */
+enum real M_1_PI =     0.318309886183790671537767; /** 1 / $(PI) = 0.318309... */
+enum real M_2_PI =     0.636619772367581343075535; /** 2 / $(PI) = 0.636619... */
+enum real M_2_SQRTPI = 1.128379167095512573896158; /** 2 / $(SQRT)$(PI) = 1.128379... */
+enum real SQRT2 =      1.414213562373095048801688; /** $(SQRT)2 = 1.414213... */
+enum real SQRT1_2 =    0.707106781186547524400844; /** $(SQRT)$(HALF) = 0.707106... */
 // Note: Make sure the magic numbers in compiler backend for x87 match these.
 
 
